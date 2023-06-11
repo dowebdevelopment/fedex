fb3c2e598f8ed8ef0fa71612615addbcc21d5741
Dennis Otten <dennis@dowebdevelopment.nl>
2023-06-11 12:00:54 +0200

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SignupComponent } from './components/forms/signup/signup.component';
-import { InputComponent } from './components/elements/input/input.component';
+import { SignupPageComponent } from './components/pages/signup/signup-page.component';
+import { SignupFormComponent } from './components/forms/signup/signup-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent,
-    InputComponent
+    SignupPageComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule
